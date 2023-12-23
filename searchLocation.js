8ebd8c035c9d2055a08bc09a454a66194828e23b
hamza.siddiqui <hamzahmedsiddiqui@outlook.com>
2023-12-23 15:34:53 -0500

    chore: addition of working code using google api

--- searchLocation.js
+++ searchLocation.js
@@ -1,21 +1,27 @@
 const axios = require('axios');
 
-async function getPlaceDetails(placeName) {
-  try {
-    const response = await axios.get(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(placeName)}`);
-    
-    if (response.data && response.data.length > 0) {
-      const firstResult = response.data[0];
-      const addressDetails = firstResult.display_name;
+const GOOGLE_MAPS_API_KEY = 'AIzaSyDjb_zVAsoBOhyrKpBDACCGUIX4W8yQr2E';
 
-      console.log(`Address Details: ${addressDetails}`);
-    } else {
-      console.log(`Could not find information for ${placeName}.`);
+async function getCountryFromAddress(address) {
+    try {
+        const response = await axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${GOOGLE_MAPS_API_KEY}`);
+        
+        if (response.data && response.data.results && response.data.results.length > 0) {
+            const firstResult = response.data.results[0];
+            const addressDetails = firstResult.address_components;
+
+            // Extract country information
+            const countryComponent = addressDetails.find(component => component.types.includes('country'));
+            const countryName = countryComponent ? countryComponent.long_name : null;
+
+            return countryName;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error('Error:', error.response ? error.response.data.error_message : error.message);
+        return null;
     }
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
 }
 
-// Replace 'YourPlaceName' with the actual place name you want to look up
-getPlaceDetails('University of Essex');
+module.exports = { getCountryFromAddress };
